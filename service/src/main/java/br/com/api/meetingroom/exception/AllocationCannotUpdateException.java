0522ddf867fb8774d2e07b3f5f8e85578764e650
Victor Simião <victor.reis@zup.com.br>
2022-03-22 14:27:24 -0300

    Refatorando API

--- service/src/main/java/br/com/api/meetingroom/exception/AllocationCannotUpdateException.java
+++
@@ -1,7 +0,0 @@
-package br.com.api.meetingroom.exception;
-
-public class AllocationCannotUpdateException extends RuntimeException {
-    public AllocationCannotUpdateException(String message) {
-        super(message);
-    }
-}
